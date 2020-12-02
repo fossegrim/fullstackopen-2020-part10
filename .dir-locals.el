f55235e69e046350e10866fff50219aedbf6adc9
Olav Fosse <mail@olavfosse.no>
2020-12-02 12:52:23 +0100

    Set (emacs) js-mode and js-jsx-mode indentation settings

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((js-jsx-mode . ((js-jsx-indent-level . 2)))
+ (js-mode . ((js-indent-level . 2))))
